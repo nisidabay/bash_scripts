d5ccaba02259040953dee05883db5a26fea1453f
nisidabay <nisidabay@gmail.com>
2024-01-23 06:46:23 +0100

    Add sysadmin/harmful_http_post.sh

---
+++ sysadmin/harmful_http_post.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+INTERFACE="enp2s0"  # "eth0" or "wlan0" or "enp2s0" or other interface names as per your system
+
+
+# Run tcpdump to listen for POST requests with suspicious commands
+sudo tcpdump -i $INTERFACE port 80 -A | grep -i "POST" | \
+grep -Ei "(eval|system|exec|bash|sh|wget|curl)" | \
+awk '{print $8}'
+
+# Why It Is Useful:
+
+# This command sequence is particularly useful for cybersecurity monitoring. A
+# Blue Team, which is tasked with defending against cyber attacks, can use this
+# script to:
+
+# Monitor network traffic for potentially harmful HTTP POST requests that might be
+# trying to exploit vulnerabilities like remote command execution or script
+# injection.
+
+# Detect suspicious commands that could indicate an attempted attack or a
+# compromised system.
+
+# Quickly gather data that may be required for incident response or forensic
+# analysis.
+
+# This kind of monitoring is crucial for maintaining the security of web servers
+# and services, especially those that are publicly accessible and therefore more
+# prone to attack. By identifying such patterns in network traffic, security teams
+# can take appropriate actions to investigate and respond to potential threats.
+
