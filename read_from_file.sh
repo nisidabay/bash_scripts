6256e2777ccb26d45f634b15ec8c52c8e2451647
nisidabay <nisidabay@gmail.com>
2024-01-26 11:12:26 +0100

    Modify read_from_file.sh

--- read_from_file.sh
+++ read_from_file.sh
@@ -1,9 +1,9 @@
-#/bin/bash
+#!/bin/bash
 echo "Enter the file name: "
-read filename
-[ -f $filename ] || echo "($filename) does not exist"
+read -r filename
+[ -f "$filename" ] || echo "($filename) does not exist"
 
 while IFS= read -r line
 do
 	echo "$line"
-done<$filename
+done < "$filename"
