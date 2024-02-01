9360d7654ab7f1ebc3b8aae4d62a068701e3cedc
nisidabay <nisidabay@gmail.com>
2024-02-01 18:31:04 +0100

    Add sourcing_example.sh

---
+++ sourcing_example.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+# 
+# Sourcing a script in Bash
+
+#  With the "source" command we make the changes visible in the current Bash
+#  shell
+
+# Create a Bash script named script.sh
+echo 'export MY_VARIABLE="Hello from script"' > script.sh
+
+# Source the script within the current Bash shell
+source script.sh
+
+# Access the variable set in the sourced script
+echo "MY_VARIABLE is set to: $MY_VARIABLE"
