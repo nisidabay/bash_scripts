43557cb522382f33097d9280138734c0979f6ec9
nisidabay <nisidabay@gmail.com>
2024-02-15 22:04:03 +0100

    Modify user_confirm.sh

--- user_confirm.sh
+++ user_confirm.sh
@@ -1,54 +1,45 @@
-#!/usr/bin/bash
-##############################################################################
-# Idea from: Groctel
-# Author:  Groctel
-# Description: Ask user for confimation before taking an action
-# Date:
-# Dependencies:
-##############################################################################
-function Confirm ()
-{
-	answer=-1
+#!/bin/bash
+# 
+# Quit function. Ask the user for an action
 
-	case "$1" in
-	[Yy][Ee][Ss])
-		printf "\033[1;32m:: \033[0m%s? \033[1;32m[Y/n]:\033[0m " "$2"
-	;;
-	[Nn][Oo])
-		printf "\033[1;31m:: \033[0m%s? \033[1;31m[y/N]:\033[0m " "$2"
-	;;
-	 *)
-		 printf "\033[1;33m:: \033[0m%s? \033[1;33m[y/n]:\033[0m " "$1"
-	 ;;
-	esac
+Quit () {
+    # Declare local variables for the function
+    local prompt default yn
 
-	while [ $answer -eq -1 ]
-	do
-		read -r yn
+    # Set the prompt and default value based on the first argument ($1)
+    case "$1" in
+        # If the first argument starts with Y or y, set the prompt to "[Y/n]" and default to Y
+        [Yy]*) prompt="[Y/n] "; default=Y;;
+        # If the first argument starts with N or n, set the prompt to "[y/N]" and default to N
+        [Nn]*) prompt="[y/N] "; default=N;;
+        # For any other value, set a neutral prompt "[y/n]" without a default
+        *) prompt="[y/n] "; default=;;
+    esac
 
-		case $yn in
-		[Yy]*)
-			answer=0
-		;;
-		[Nn]*)
-			answer=1
-		;;
-		*)
-			case "$1" in
-			[Yy][Ee][Ss])
-				answer=0
-			;;
-			[Nn][Oo])
-				answer=1
-			;;
-			esac
-		;;
-		esac
-	done
+    # Loop indefinitely until the user provides a valid response (Y/y/N/n)
+    while true; do
+        # Display the prompt to the user without a newline, waiting for input
+        printf "%s" "$prompt"
+        # Read the user's input into the variable 'yn'
+        read -r yn
+        # If the user presses Enter without typing anything, use the default value
+        if [ -z "$yn" ]; then
+            yn=$default
+        fi
 
-	return $answer
+        # Handle the user's response
+        case $yn in
+            # If the user responds with Y or y, return success (0) and exit the loop
+            [Yy]*) return 0;;
+            # If the user responds with N or n, return failure (1) and exit the loop
+            [Nn]*) return 1;;
+        esac
+    done
 }
-#--- test
-if Confirm "Yes" "Want to quit"; then
-    echo Quitting
+
+# Example usage of the Quit function
+# Test the Quit function with a prompt that defaults to Yes
+if Quit "Yes"; then
+    # If the user confirms, execute the following command
+    echo "Quitting"
 fi
