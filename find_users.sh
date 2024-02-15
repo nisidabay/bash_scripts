dcbe2ba3eeab57266fb42f5c1d5274cae8c75825
nisidabay <nisidabay@gmail.com>
2024-02-15 22:03:56 +0100

    Modify find_users.sh

--- find_users.sh
+++ find_users.sh
@@ -1,21 +1,38 @@
-#!/bin/bash
+#!/bin/sh
+# 
+# Check if user exists
 
-# Author: Carlos Lacaci Moya
-# Descripción: Comprueba usuarios en un sistema antes de 
-# asignarles una carpeta en /home
-# Fecha: 09/05/2020
+CheckUser() {
+    local user="$1"
 
+    # Determine the operating system
+    local os=$(uname)
 
-echo [+] This script will find users with home folder in the system
-cd /home
-for DIR in *
-do
-	echo [+] Find user: $DIR
-	check=$(grep -c $DIR /etc/passwd)
-	if [ $check -ge 1 ]
-	then
-		echo [!] $DIR exists already
-	else
-		echo [+] $DIR can be assigned
-	fi
-done
+    # For Linux systems
+    if [ "$os" = "Linux" ]; then
+        if grep -q "^$user:" /etc/passwd; then
+            return 0  # User exists
+        else
+            return 1  # User does not exist
+        fi
+    # For macOS systems
+    elif [ "$os" = "Darwin" ]; then
+        if dscl . -list /Users | grep -q "^$user\$"; then
+            return 0  # User exists
+        else
+            return 1  # User does not exist
+        fi
+    else
+        echo "Unsupported operating system."
+        return 2  # Unsupported OS
+    fi
+}
+
+# Example usage of the function
+user="nisidabay"  # Replace this with the username you want to check
+
+if ! CheckUser "$user"; then 
+    echo "The user does not exist."
+else
+    echo "The user exists."
+fi
