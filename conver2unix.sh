9fcecb996af2704f6abbbdf9c2355c6b828bb15f
nisidabay <nisidabay@gmail.com>
2024-12-28 17:16:55 +0100

    Modify files

--- conver2unix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for x
-	do
-		echo "Converting to Unix $x"
-		tr '\015' '\012' < "$x" > "tmp.$x"
-		mv "tmp.$x" "$x"
-	done		
--- zenity_info.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-zenity --info --text "This will open a terminal in this directory"
-xfce4-terminal
