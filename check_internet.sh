8bd8598d5a50b6ac3b877dff0f604fce1f5d68cf
nisidabay <nisidabay@gmail.com>
2025-03-01 09:06:42 +0100

    Modify check_internet.sh

--- check_internet.sh
+++ check_internet.sh
@@ -2,17 +2,28 @@
 #
 # Checks that an internet connection exists.
 #
+# Function to check and install a package
+check_and_install() {
+    local package=$1
+    if ! pacman -Qi "$package" &>/dev/null; then
+        echo "$package is not installed. Installing..."
+        sudo pacman -S "$package"
+    fi
+}
 
-function Main() {
+# Check for necessary packages
+check_and_install curl
+
+function main() {
     local default=https://duckduckgo.com
 
     /usr/bin/curl --silent --fail --connect-timeout 8 "$default" >/dev/null
-
-    if [  $? -eq 0 ];then
+    status="$?"
+    if [ $status -eq 0 ]; then
         echo "Internet is ON"
     else
         echo "Internet is OFF"
-    fi 
+    fi
 }
 
-Main 
+main
